679578c5ee2f126706373560754375937a11c79f
纷羽 <gzj0078@icloud.com>
2019-01-20 17:39:50 +0800

    首页布局初始化

---
+++ src/page/index/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../../assets/iconfont/iconfont.css";
+
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Microsoft YaHei" "Franklin Gothic Medium", "Arial Narrow", Arial,
+    sans-serif;
+}
+img {
+  user-select: none;
+}
+</style>
---
+++ src/page/index/views/BlobCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Blob / 个人博客</h2>
+    <div v-for="blobhref in bloblist" :key="blobhref.id">
+      <a>{{blobhref.tittle}}</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      bloblist: [
+        {
+          id: 1,
+          tittle: "Python3 与 node-sass不兼容问题和解决方案"
+        },
+        {
+          id: 2,
+          tittle: "第十届蓝桥杯参赛总结"
+        },
+        {
+          id: 3,
+          tittle: "教职工晚会炸了3台服务器的事故总结"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
